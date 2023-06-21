faf2f880412ab73231f9aee9ed9e0433902cf3a3
xjf7711 <xjf7711@qq.com>
2023-06-21 09:13:38 +0800

    type node

--- type-dom/type-node/type-node.interface.ts
+++ type-dom/type-node/type-node.interface.ts
@@ -8,12 +8,24 @@ export interface IPath {
   name: string,
   pos: number,
 }
+
+/**
+ * json格式的接口，也是json存储的数据结构
+ */
 export interface ITypeNode {
   nodeName: string;
   className?: string;
+  // propObj与attributes应该可以相互转换，而且都是只在TypeElement和XNode中有。
+  // TextNode 没有 attributes和propObj
   propObj?: ITypeProperty;
+  // nodeValue只在 TextNode中才有。
   nodeValue?: string;
   parentNode?: ITypeNode | null;
+  // TextNode 没有 childNodes
   childNodes?: ITypeNode[];
   attributes?: INodeAttr[];
+  // 绑定的事件集合, TypeElement 才有
+  // 生成json时，基于events生成；
+  // 反向转为类时，要转为events的值
+  on?: Record<string, string>;
 }
