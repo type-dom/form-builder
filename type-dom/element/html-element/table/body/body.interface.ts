af4215606d97cb36ff486460a5655187969b9087
xu jianfeng <jianfeng.xu@cssca.com>
2023-07-03 12:32:03 +0800

    table edit

--- type-dom/element/html-element/table/body/body.interface.ts
+++ type-dom/element/html-element/table/body/body.interface.ts
@@ -1,6 +1,5 @@
 import { ITypeTableBody } from '../../../../type-element/type-html/table/body/body.interface';
 import { ITableRow } from '../row/row.interface';
-
 export interface ITableBody extends ITypeTableBody {
   className: 'TableBody',
   childNodes: ITableRow[],
--- type-dom/element/html-element/table/head/head.class.ts
+++ type-dom/element/html-element/table/head/head.class.ts
@@ -1,7 +1,3 @@
-import { fromEvent } from 'rxjs';
-import { AppRoot } from '../../../../../src/app-root';
-import { TextNode } from '../../../../text-node/text-node.class';
-import { ITableField } from '../../../../../src/core/controls/complex/table/table.interface';
 import { TypeTableHead } from '../../../../type-element/type-html/table/head/head.class';
 import { TableHeaderCell } from '../header-cell/header-cell.class';
 import { Table } from '../table.class';
@@ -11,49 +7,9 @@ import { ITableHead } from './head.interface';
 export class TableHead extends TypeTableHead implements ITableHead {
   className: 'TableHead';
   childNodes: TableHeaderCell[];
-
-  constructor(public parent: Table, th: ITableField[] = []) {
+  constructor(public parent: Table) {
     super();
-    this.nodeName = 'thead';
-    this.dom = document.createElement(this.nodeName);
     this.className = 'TableHead';
     this.childNodes = [];
-    this.setHeadItems(th);
-    this.initEvents();
-  }
-
-  setHeadItems(th: ITableField[]): void {
-    this.clearChildDom();
-    this.clearChildNodes();
-    for (const field of th) {
-      const tableHeader = new TableHeaderCell(this);
-      const text = new TextNode(tableHeader, field.label);
-      tableHeader.setAttrName(field.name);
-      tableHeader.childNodes.push(text);
-      this.childNodes.push(tableHeader);
-    }
-  }
-
-  createInstance(tHeadLiteral: ITableHead): void {
-    if (tHeadLiteral.propObj) {
-      this.setPropObj(tHeadLiteral.propObj);
-    }
-    tHeadLiteral.childNodes.forEach((child, index) => {
-      if (this.childNodes[index]) {
-        this.childNodes[index].createInstance(child);
-      } else {
-        const th = new TableHeaderCell(this);
-        th.createInstance(child);
-        this.appendChild(th);
-      }
-
-    });
-  }
-  initEvents(): void {
-    this.events.push(
-      fromEvent(this.dom, 'click').subscribe(() => {
-        AppRoot.setSelectedTableDataCell(null);
-      })
-    );
   }
 }
