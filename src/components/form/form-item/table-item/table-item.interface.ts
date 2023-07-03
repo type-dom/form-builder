de1bdb9fce92937d8f8df25a89c3cad6bda09e65
xu jianfeng <jianfeng.xu@cssca.com>
2023-07-03 12:29:32 +0800

    table item import

--- src/components/form/form-item/table-item/table-item.interface.ts
+++ src/components/form/form-item/table-item/table-item.interface.ts
@@ -1,7 +1,7 @@
 import { ILabel } from '../../../../../type-dom/element/html-element/label/label.interface';
-import { ITable } from '../../../../../type-dom/element/html-element/table/table.interface';
 import { ISpan } from '../../../../../type-dom/element/html-element/span/span.interface';
 import { IFormItem } from '../form-item.interface';
+import { ITable } from './table/table.interface';
 
 export interface ITableItem extends IFormItem {
   className: 'TableItem',
