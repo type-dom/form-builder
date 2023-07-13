5cdaa4a36fae629e8b0f75cbeb01206a1d33190e
xu jianfeng <jianfeng.xu@cssca.com>
2023-07-13 12:28:44 +0800

    chang tablesvg and port

--- src/core/menus/complex-menus/table/table-menu.ts
+++ src/core/menus/complex-menus/table/table-menu.ts
@@ -1,13 +1,13 @@
 import { Span } from 'type-dom.ts';
-import { TdTableSvg } from 'type-dom-svgs';
+import { TableSvg } from 'type-dom-svgs';
 import { TableControl } from '../../../controls/complex/table/table.class';
 import { ControlMenu } from '../../menu.abstract';
 import { ComplexMenus } from '../complex-menus';
 // import html from './table-menu.html';
 export class TableMenu extends ControlMenu {
   className: 'TableMenu';
-  childNodes: [TdTableSvg, Span];
-  svg: TdTableSvg;
+  childNodes: [TableSvg, Span];
+  svg: TableSvg;
   ControlClass: typeof TableControl;
 
   constructor(public parent: ComplexMenus) {
@@ -15,7 +15,7 @@ export class TableMenu extends ControlMenu {
     this.className = 'TableMenu';
     this.ControlClass = TableControl;
     this.addAttrName('table-menu');
-    this.svg = new TdTableSvg(this);
+    this.svg = new TableSvg(this);
     this.svg.resetSize(24, 24);
     this.textNode.setText('表格');
     this.childNodes = [this.svg, this.titleSpan];
